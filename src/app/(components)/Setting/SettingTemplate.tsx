d26b93de64f3ddc6973e3c80d73d007f0447f925
Sho8000 <yoshikichi8000@gmail.com>
2025-08-07 11:55:20 -0700

    fixed closebtn position

--- src/app/(components)/Setting/SettingTemplate.tsx
+++ src/app/(components)/Setting/SettingTemplate.tsx
@@ -22,13 +22,13 @@ export default function SettingTemplate({title}:SettingProps) {
     <>
       {isSettingPage && 
         <div className="w-[100vw] h-[100vh] bg-black/80 top-0 left-0 z-50 fixed">
+          <div className={`absolute top-0 right-0 translate-y-[50%] translate-x-[-10%] z-50`}>
+            <CloseBtn clickFunction={closeBtnHandler}/>
+          </div>
           <div className={`relative top-[50%] left-[50%] translate-[-50%] rounded-[10px] py-[1rem] bg-[#fdfdfd]
             ${Styles.whiteBox}  
           `}>
             <SectionTitle text={title}/>
-            <div className={`absolute top-0 right-0 translate-y-[-50%] translate-x-[50%]`}>
-              <CloseBtn clickFunction={closeBtnHandler}/>
-            </div>
 
             {/* each Menu area */}
             <SettingMenu/>

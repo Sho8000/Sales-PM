6d3f96ad7b249e75c364f5cc1d6ba9f7c3197aa0
Sho8000 <yoshikichi8000@gmail.com>
2025-08-08 19:51:25 -0700

    added title and appointment date into prospects:id page

--- src/app/(components)/CommonParts/UpComing.tsx
+++ src/app/(components)/CommonParts/UpComing.tsx
@@ -1,17 +1,21 @@
-import { getNextAppointmentDateFromAllData, NextAppointmentWithProspect } from "@/lib/nextAppointment";
+import { getNextAppointmentDateFromAllData, getNextAppointmentDateFromNotes, NextAppointmentWithProspect } from "@/lib/nextAppointment";
 import Styles from "./CommonParts.module.css"
 import { useUserInfoStore } from "@/store/userInfoStore";
 import { useEffect, useState } from "react";
-import { Prospects } from "@/lib/dbInterface";
+import { Notes, Prospects } from "@/lib/dbInterface";
+import { useClickedProspectInfoStore } from "@/store/clickedProspectsInfoStore";
 
 interface TitleProps {
   text:"Upcoming"|"Appointment";
-  clickFunction: (prospectInfo:Prospects)=>void;
+  clickFunction?: (prospectInfo:Prospects)=>void;
 }
 
 export default function UpComing({text,clickFunction}:TitleProps) {
   const userData = useUserInfoStore((state) => state.user); //use user's all Information
+  const clickedProspectData = useClickedProspectInfoStore((state) => state.prospect); //use clicked prospect's Information
+
   const [earlistAppointmentInfo,setEarlistAppointmentInfo] = useState<NextAppointmentWithProspect|null>(null);
+  const [appointmentDate,setAppointmentDate] = useState<Notes|null>(null)
 
   useEffect(()=>{
     if(userData?.prospectList?.prospects){
@@ -19,30 +23,87 @@ export default function UpComing({text,clickFunction}:TitleProps) {
     }
   },[userData])
 
+  useEffect(()=>{
+    if(clickedProspectData?.notes){
+      const earlistDate = getNextAppointmentDateFromNotes(clickedProspectData?.notes)
+      setAppointmentDate(earlistDate);
+    }
+  },[clickedProspectData])
+
   return (
     <div className={`w-full font-bold mb-[1rem] ${Styles.textFont}`}>
       <h2 className={`w-full text-center ${Styles.forPC}`}>
         {text} :
-        {earlistAppointmentInfo?
-          <span className="text-red-500 underline underline-offset-8" onClick={()=>clickFunction(earlistAppointmentInfo.prospect)}> {earlistAppointmentInfo.prospect.prospectName} : {new Date(earlistAppointmentInfo.note.appointmentDate).toLocaleString("en-US", {
-            year: "numeric",
-            month: "short",
-            day: "numeric",
-          })}</span>
-          :<span className="text-gray-400"> No Appointment</span>
+        {text==="Upcoming" ?
+          <>
+            {earlistAppointmentInfo?
+              <span className="text-red-500 underline underline-offset-8"
+              onClick={()=>{
+                if(clickFunction){
+                  clickFunction(earlistAppointmentInfo.prospect)}
+                }
+              }
+              >
+                {earlistAppointmentInfo.prospect.prospectName} : {new Date(earlistAppointmentInfo.note.appointmentDate).toLocaleString("en-US", {
+                  year: "numeric",
+                  month: "short",
+                  day: "numeric",
+                })}
+              </span>
+              :<span className="text-gray-400"> No Appointment</span>
+            }
+          </>
+          :<>
+            {appointmentDate?
+              <span className="text-red-500 underline underline-offset-8"
+              >
+                {new Date(appointmentDate.appointmentDate).toLocaleString("en-US", {
+                  year: "numeric",
+                  month: "short",
+                  day: "numeric",
+                })}
+              </span>
+              :<span className="text-gray-400"> No Appointment</span>
+            }
+          </>
         }
       </h2>
       <div className={`${Styles.forPhone}`}>
         <h2 className={`w-full text-center`}>
           {text}
         </h2>
-        {earlistAppointmentInfo?
-          <h2 className="text-red-500 underline text-center underline-offset-8" onClick={()=>clickFunction(earlistAppointmentInfo.prospect)}> {earlistAppointmentInfo.prospect.prospectName} : {new Date(earlistAppointmentInfo.note.appointmentDate).toLocaleString("en-US", {
-            year: "numeric",
-            month: "short",
-            day: "numeric",
-          })}</h2>
-          :<h2 className="font-medium text-center text-gray-400">No Appointment</h2>
+        {text==="Upcoming" ?
+          <>
+            {earlistAppointmentInfo?
+              <h2 className="text-red-500 underline text-center underline-offset-8"
+              onClick={()=>{
+                if(clickFunction){
+                  clickFunction(earlistAppointmentInfo.prospect)}
+                }
+              }
+              >
+                {earlistAppointmentInfo.prospect.prospectName} : {new Date(earlistAppointmentInfo.note.appointmentDate).toLocaleString("en-US", {
+                  year: "numeric",
+                  month: "short",
+                  day: "numeric",
+                })}
+              </h2>
+              :<h2 className="font-medium text-center text-gray-400">No Appointment</h2>
+            }
+          </>
+          :<>
+            {appointmentDate?
+              <h2 className="text-red-500 underline text-center underline-offset-8"
+              >
+                {new Date(appointmentDate.appointmentDate).toLocaleString("en-US", {
+                  year: "numeric",
+                  month: "short",
+                  day: "numeric",
+                })}
+              </h2>
+              :<h2 className="font-medium text-center text-gray-400">No Appointment</h2>
+            }
+          </>
         }
       </div>
     </div>
--- src/app/prospectslist/[id]/page.tsx
+++ src/app/prospectslist/[id]/page.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import SectionTitle from "@/app/(components)/CommonParts/SectionTitle";
+import UpComing from "@/app/(components)/CommonParts/UpComing";
 import { useClickedProspectInfoStore } from "@/store/clickedProspectsInfoStore";
 
 export default function ProspectInfo() {
@@ -10,6 +12,11 @@ export default function ProspectInfo() {
     <>
       {prospectData &&
         <>
+          <SectionTitle text={"Prospect Info"}
+             
+          />
+          <UpComing text="Appointment"/>
+        
           <h1>{prospectData.prospect?.prospectName}</h1>
           <h1>{prospectData.prospect?.prospectEmail}</h1>
         </>
